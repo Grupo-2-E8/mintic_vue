1303cc319d9d958cacf20e3ccca086217e5f7615
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2021-09-16 21:20:48 -0500

    agregando primevue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+app.use(PrimeVue)
+
 
 createApp(App).use(store).use(router).mount('#app')
