c506c96ed21c200a8d1ef06a6c27523a5ad0b399
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2021-09-16 21:52:36 -0500

    agregando sidebar

--- src/views/clientes/index.vue
+++ src/views/clientes/index.vue
@@ -1,15 +1,57 @@
 <template>
   <section>
-    <div class="">
-      <h1>Mis clientes</h1>
-    </div>
-    <DataTable 
-    :value="getClientes"
+    <Sidebar v-model:visible="visibleLeft">
+      <div class="d-flex w-100 flex-column">
+        <router-link :to="{name: 'clientes'}" class="w-100 mb-2">
+          Clientes
+        </router-link>
+        <router-link :to="{name: 'pedidos'}" class="w-100 mb-2">
+          Pedidos
+        </router-link>
+      </div>
+    </Sidebar>
+    <button 
+    type="button" 
+    class="btn btn-primary mt-2"
+    @click="handleMenuToggle"
     >
-      <Column header="nombres" field="nombres"  sortable />
-      <Column header="identificacion" field="identificacion"  />
-      <Column header="Correo electronico" field="correo"  />
-    </DataTable>
+      Ver menu
+    </button>
+    <div class="d-flex w-100 p-4">
+      <div class="card my-3">
+        <div class="card-body">
+          <h1>Mis clientes</h1>
+        </div>
+      </div>
+    </div>
+    <div class="p-4">
+      <DataTable 
+      :value="getClientes"
+      >
+        <Column header="nombres" field="nombres"  sortable />
+        <Column 
+        header="identificacion" 
+        field="identificacion"  
+        />
+        <Column header="Correo electronico" field="correo"  />
+        <Column header="Acciones">
+          <template #body>
+            <button 
+            type="button" 
+            class="btn btn-danger m-1"
+            >
+              Eliminar
+            </button>
+            <button 
+            type="button" 
+            class="btn btn-primary m-1"
+            >
+              Editar
+            </button>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
   </section>
 </template>
 
@@ -17,12 +59,16 @@
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import {computed, ref} from 'vue'
+import Sidebar from 'primevue/sidebar';
+
 export default {
   components:{
     DataTable,
-    Column
+    Column,
+    Sidebar
   },
   setup(){
+    const visibleLeft = ref(false)
     const clientes = ref([
       {
         nombres: 'pepito perez',
@@ -36,8 +82,13 @@ export default {
       }
     ])
     const getClientes = computed(() => clientes.value)
+    const handleMenuToggle = ()=>{
+      visibleLeft.value = !visibleLeft.value
+    }
     return {
-      getClientes
+      getClientes,
+      visibleLeft,
+      handleMenuToggle
     }
   }
 
