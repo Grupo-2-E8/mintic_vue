52cf329f14aa93ed9076094aa29e1eeceb8432e8
Camilo <cammilo.hernandez@gmail.com>
2021-10-06 20:49:41 -0500

    Formulario Empresa

--- src/views/clientes/form/index.vue
+++ src/views/clientes/form/index.vue
@@ -1,25 +1,48 @@
 <template>
-  <section>
-    <div class="container ">
-      <div class="row justify-content-center">
-        <div class="col-md-5 border">
-          <form action="">
-            <div class="form-group mb-2 text-left">
-              <label for="" class="text-left">Nombres</label>
-              <input type="text" class="form-control">
-            </div>
-            <div class="form-group mb-2 text-left">
-              <label for="" class="text-left">Apellidos</label>
-              <input type="text" class="form-control">
-            </div>
-            <div class="form-group mb-2 text-left">
-              <label for="" class="text-left">Identificacion</label>
-              <input type="text" class="form-control">
-            </div>
-          </form>
+<section>
+<form method="POST" action="/index.php" class="needs-validation" novalidate>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-4 mb-3">
+            <label for="validarNombre">Nombre:<span class="red">*</span></label>
+            <input type="text" class="form-control" id="validarNombre" name="validarNombre" required>
         </div>
-      </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <label for="validarApellidos">Apellidos:<span class="red">*</span></label>
+            <input type="text" class="form-control" id="validarApellidos" name="validarApellidos" required>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <label for="validarEmail">Email:<span class="red">*</span></label>
+            <input type="email" class="form-control" id="validarEmail" name="validarEmail" required>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <label for="validarTelefono">Teléfono:</label>
+            <input type="number" class="form-control" id="validarTelefono" name="validarTelefono" max="999999999">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 mb-3">
+            <label for="validarAsunto">Asunto:</label>
+            <input type="text" class="form-control" id="validarAsunto" name="validarAsunto" required>
+        </div>
+    </div>
+
+    <div class="group mb-10">
+        <button class="btn btn-primary" type="submit" name="submit">Enviar</button>
+        <button class="btn btn-success" type="reset" name="reset">Limpiar</button>
+    </div>
+  </div>
+</form>
   </section>
 </template>
 
