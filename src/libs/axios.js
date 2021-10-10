ede9644f4800afb19698117dbb1809c0601f1ad1
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2021-10-09 20:10:34 -0500

    configurando interceptores

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,6 +1,33 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
+import Swal from 'sweetalert2'
 import app from '../config/app.config'
+import store from '../store'
 
 axios.defaults.baseURL = `${app.api}/api`
+
+axios.interceptors.request.use(config=>{
+  const token = Cookies.get('token')
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+axios.interceptors.response.use(response => response,error=>{
+  const { status } = error
+  if(status === 401 &&  store.getters['auth/check']){
+    Swal.fire('Advertencia','Esta sesion a expirado','warning').then(()=>{
+      store.commit('auth/LOGOUT')
+      router.push({ name: 'auth.login' })
+    })
+  } 
+  
+  if (status >= 500) {
+    Swal.fire('Mensaje','Ha sucedido un error','error')
+  }
+
+  return Promise.reject(error)
+})
 
 export default axios
