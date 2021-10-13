ec9cc6a6437bf3646d4d707cfc80e4a971a27b74
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2021-10-12 21:22:44 -0500

    control de estados en linea de tiempo

--- src/views/pedidos/index.vue
+++ src/views/pedidos/index.vue
@@ -22,7 +22,7 @@
           <template #body="{data}">
             <button 
             type="button" 
-            class="btn btn-primary py-0"
+            class="btn btn-primary p-1 m-1"
             title="Ver"
             @click="handleView(data)"
             >
@@ -30,10 +30,76 @@
                 <path fill="currentColor" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
               </svg>
             </button>
+            <button 
+            type="button"
+            class="btn btn-warning p-1 m-1"
+            title="Cambiar Estado"
+            @click="handleEstadoChange(data)"
+            >
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M21.04 12.13C21.18 12.13 21.31 12.19 21.42 12.3L22.7 13.58C22.92 13.79 22.92 14.14 22.7 14.35L21.7 15.35L19.65 13.3L20.65 12.3C20.76 12.19 20.9 12.13 21.04 12.13M19.07 13.88L21.12 15.93L15.06 22H13V19.94L19.07 13.88M11 19L9 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H9.18C9.6 1.84 10.7 1 12 1C13.3 1 14.4 1.84 14.82 3H19C20.1 3 21 3.9 21 5V9L19 11V5H17V7H7V5H5V19H11M12 3C11.45 3 11 3.45 11 4C11 4.55 11.45 5 12 5C12.55 5 13 4.55 13 4C13 3.45 12.55 3 12 3Z" />
+              </svg>
+            </button>
           </template>
         </Column>
       </DataTable>
     </div>
+    <Dialog 
+    header="Cambiar estado" 
+    :modal="true"
+    v-model:visible="display" 
+    >
+      <form @submit="handleForm">
+        <div class="row">
+          <div class="col-12 mb-3">
+            <label for="estado">Estado</label>
+            <select 
+            id="estado"
+            v-model="model.estado"
+            class="form-control"
+            >
+              <option value="Pago recibido">Pago recibido </option>
+              <option value="En fabricacion">En fabricacion</option>
+              <option value="En empaque">En empaque</option>
+              <option value="En transportadora">En transportadora</option>
+              <option value="recibido por el cliente">recibido por el cliente</option>
+            </select>
+            <div class="text-danger">
+              {{errors.estado}}
+            </div>
+          </div>
+          <div class="col-12 mb-3">
+            <label for="justificacion">Justificacion</label>
+            <textarea
+            id="justificacion"
+            v-model="model.justificacion"
+            rows="3" 
+            class="form-control" 
+            />
+            <div class="text-danger">
+              {{errors.justificacion}}
+            </div>
+          </div>
+          <div class="col-12 mb-3">
+            <button 
+            type="submit"
+            class="btn btn-primary m-1"
+            :disabled="esProcesando"
+            >
+              Guardar
+            </button>
+            <button 
+            type="button"
+            class="btn btn-outline-warning m-1"
+            @click="display = !display"
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </form>
+    </Dialog>
+
   </Page>
 </template>
 
@@ -42,19 +108,28 @@ import Page from '../../components/Page.vue'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import {onMounted, ref} from 'vue'
-import {pedidosListar} from './service'
+import {pedidosListar,pedidosActualizar} from './service'
 import { useRouter } from 'vue-router'
+import Dialog from 'primevue/dialog';
+import {useField,useForm} from 'vee-validate'
+import {object,string} from 'yup'
+import Swal from 'sweetalert2'
 
 export default {
   name: 'PedidosPage',
   components:{
     Page,
     DataTable,
-    Column
+    Column,
+    Dialog
   },
   setup(){
-    const pedidos = ref([])
     const router = useRouter()
+    const pedidos = ref([])
+    const display = ref(false)
+    const pedidoSel = ref({})
+    const esProcesando = ref(false)
+
     const pedidosFetch = ()=>{
       return pedidosListar().then(({data})=>{
         pedidos.value = data
@@ -62,6 +137,7 @@ export default {
         console.error(err.message);
       })
     }
+    
     const handleView = ({_id:id}) => {
       router.push({
         name: 'pedidos.ver',
@@ -69,12 +145,54 @@ export default {
       })
     }
 
+    const handleEstadoChange = (value) => {
+      display.value = !display.value
+      pedidoSel.value = {...value}
+    }
+    
+    const validationSchema = object({
+      estado: string().required(),
+      justificacion: string().required()
+    })
+
+    const {
+      values: model,
+      handleSubmit,
+      errors
+    } = useForm({
+      validationSchema
+    })
+
+    useField('estado',null,{initialValue: ''})
+    useField('justificacion',null,{initialValue: ''})
+
+    const handleForm = handleSubmit((values)=>{
+      esProcesando.value = true
+      const {_id:id} = pedidoSel.value
+      pedidosActualizar(id,values).then(({data})=>{
+        display.value = false
+        esProcesando.value = false
+        Swal.fire('Mensaje','Estado agregado','success')
+      }).catch(err=>{
+        console.error(err.message)
+        esProcesando.value = false
+      })
+    })
+
     onMounted(()=>{
       pedidosFetch()
     })
+
     return {
+      errors,
+      model,
+      pedidoSel,
+      display,
       pedidos,
-      handleView
+      esProcesando,
+      handleView,
+      handleEstadoChange,
+      handleForm
     }
   }
 }
--- src/views/pedidos/service.js
+++ src/views/pedidos/service.js
@@ -3,9 +3,11 @@ import http from '../../libs/axios'
 const pedidosCrear = (payload) => http.post('/pedidos',payload)
 const pedidosListar = () => http.get('/pedidos')
 const pedidosBuscar = (id) => http.get(`/pedidos/${id}`)
+const pedidosActualizar = (id,payload) => http.put(`/pedidos/${id}`,payload)
 
 export {
   pedidosCrear,
   pedidosListar,
-  pedidosBuscar
+  pedidosBuscar,
+  pedidosActualizar
 }
